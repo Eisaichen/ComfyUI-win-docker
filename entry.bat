86f4cf0d9c11f3c00fa9e2dccf4dd4b77525cb94
Eisai <12467320+Eisaichen@users.noreply.github.com>
2024-09-16 12:13:39 -0500

    Update entry.bat

--- entry.bat
+++ entry.bat
@@ -1,4 +1,7 @@
 @robocopy C:\app\ComfyUI\_models C:\app\ComfyUI\models /E /XC /XN /XO /R:3 /W:1 /NP >nul
 @type C:\app\README_VERY_IMPORTANT.txt
+@echo.
+@echo.
+@echo.
 @.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --listen 0.0.0.0 --disable-auto-launch %ARGS%
 @exit
