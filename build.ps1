3a28c98c2f9b47a3d883ed194c355a8e2f50d5b8
Eisai <12467320+Eisaichen@users.noreply.github.com>
2026-08-16 01:44:14 -0700

    Enhance docker build commands with --compress option
    Added --compress option to docker build commands for better performance.

--- build.ps1
+++ build.ps1
@@ -25,9 +25,9 @@ Set-Content .\build\ComfyUI_windows_portable\README_VERY_IMPORTANT.txt $i[14..20
 # Build ltsc2022
 
 if ($env:GH_CI_LATEST -eq "true") {
-    docker build --isolation hyperv --pull --no-cache -t eisai/comfy-ui:latest -t eisai/comfy-ui:$env:GH_CI_TAG .\build
+    docker build --isolation hyperv --pull --no-cache --compress -t eisai/comfy-ui:latest -t eisai/comfy-ui:$env:GH_CI_TAG .\build
 } else {
-    docker build --isolation hyperv --pull --no-cache -t eisai/comfy-ui:$env:GH_CI_TAG .\build
+    docker build --isolation hyperv --pull --no-cache --compress -t eisai/comfy-ui:$env:GH_CI_TAG .\build
 }
 
 # Push
@@ -45,9 +45,9 @@ $i=Get-Content -Path .\build\Dockerfile
 Set-Content -Path .\build\Dockerfile -Value $($i.replace("FROM mcr.microsoft.com/windows/server:ltsc2022","FROM mcr.microsoft.com/windows/server:ltsc2025"))
 
 if ($env:GH_CI_LATEST -eq "true") {
-    docker build --isolation hyperv --no-cache --pull -t eisai/comfy-ui:ltsc2025 -t eisai/comfy-ui:$env:GH_CI_TAG-ltsc2025 .\build
+    docker build --isolation hyperv --no-cache --pull --compress -t eisai/comfy-ui:ltsc2025 -t eisai/comfy-ui:$env:GH_CI_TAG-ltsc2025 .\build
 } else {
-    docker build --isolation hyperv --no-cache --pull -t eisai/comfy-ui:$env:GH_CI_TAG-ltsc2025 .\build
+    docker build --isolation hyperv --no-cache --pull --compress -t eisai/comfy-ui:$env:GH_CI_TAG-ltsc2025 .\build
 }
 
 # Push
