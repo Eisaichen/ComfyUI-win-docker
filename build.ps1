2ee86012c190a7e9f23ce0d4a6d1a9f7909a58ae
Eisai <12467320+Eisaichen@users.noreply.github.com>
2026-08-15 20:09:08 -0700

    Change Dockerfile base image to ltsc2025
    Updated Dockerfile to use ltsc2025 base image and added cleanup step.

--- build.ps1
+++ build.ps1
@@ -30,9 +30,27 @@ if ($env:GH_CI_LATEST -eq "true") {
     docker build --isolation hyperv --pull --no-cache -t eisai/comfy-ui:$env:GH_CI_TAG .\build
 }
 
-
 # Push
-
 if ($env:GH_CI_PUSH -eq "true") {
     docker push eisai/comfy-ui -a
+}
+
+# Clean up
+docker system prune --all -f
+
+
+# Build ltsc2025
+
+$i=Get-Content -Path .\build\Dockerfile
+Set-Content -Path .\build\Dockerfile -Value $($i.replace("FROM mcr.microsoft.com/windows/server:ltsc2022","FROM mcr.microsoft.com/windows/server:ltsc2025"))
+
+if ($env:GH_CI_LATEST -eq "true") {
+    docker build --isolation hyperv --no-cache --pull -t eisai/eisai/comfy-ui:ltsc2025 -t eisai/comfy-ui:$env:GH_CI_TAG-ltsc2025 .\build
+} else {
+    docker build --isolation hyperv --no-cache --pull -t eisai/eisai/comfy-ui:$env:GH_CI_TAG-ltsc2025 .\build
+}
+
+# Push
+if ($env:GH_CI_PUSH -eq "true") {
+    docker push eisai/eisai/comfy-ui -a
 }
